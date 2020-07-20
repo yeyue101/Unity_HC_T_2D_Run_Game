278be391116183964fca68128a33e421741f8eb7
yeyue101 <67904651+yeyue101@users.noreply.github.com>
2020-07-20 21:29:01 +0800

    完成特效

---
+++ 2DRunGame/Assets/Scripts/First.cs
@@ -0,0 +1,16 @@
+﻿//單行註解
+//引用UnityEngine
+using UnityEngine;
+
+//類別 class (腳本)類別名稱
+public class First:MonoBehaviour
+{
+   //大括號內為
+   //類別 成員
+   //括號 C# 成對出現
+   //() [] {} "" '' 
+
+   //類別:定義物件需要的資料與功能
+   //成員:資料與功能
+   //資料:欄位
+}
