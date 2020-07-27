a3b0859b78c7951ed182c552311bbcafd238901a
yeyue101 <67904651+yeyue101@users.noreply.github.com>
2020-07-27 21:29:10 +0800

    認識方法與事件

---
+++ 2DRunGame/Assets/Scripts/Learn.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Learn : MonoBehaviour
+{
+    //事件：在特定時間會自動執行
+    //開始：在播放遊戲後會執行一次，初始化 血量滿
+    //輸出 print("訊息") - 將訊息輸出到儀表板
+    private void Start()
+    {
+        print("哈囉，沃德~");
+
+    }
+
+    //更新：一秒執行約60次，持續執行，例如：移動 監聽玩家輸入
+    private void Update()
+    {
+        print("嗨，我在更新");
+
+        // 呼叫方法
+        //語法：方法名稱()；
+        
+    }
+
+    // 方法：
+    // 語法
+    // 修飾詞 傳回類型 方法名稱 () {程式內容}
+    // 無傳回 void
+    // 方法名稱：Unity 習慣會用大寫開頭
+    // 事件與方法差異：事件名稱為藍色，方法為白色
+    // 事件與方法差異：事件會自動執行，方法不會，需要呼叫
+    public void Test()
+
+    //參數預設值
+    public void Drive(int speed, string direction = "前方")
+    {
+        print("時速：" + speed);
+        print("開車方向：" + direction);
+
+    }
+
+
+
+
+
+
+
+}
+
+
---
+++ 2DRunGame/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    #region 欄位
+    public float speed = 5;
+    public int jump = 350;
+    public float hp = 500;
+
+    public bool isGround;
+    public int coin;
+
+    public AudioClip soundHit;
+    public AudioClip soundSlide;
+    public AudioClip soundJump;
+    public AudioClip soundCoin;
+
+    public Animator ani;
+    public Rigdbody2D rid;
+    public CapsuleCollider2D cap;
+    #endregion
+
+
+    #region 方法
+    private void Move()
+    {
+
+    }
+
+    private void Jump()
+    {
+
+    }
+
+    private void Slide()
+    {
+
+    }
+
+    private void EatCoin()
+    {
+
+    }
+
+    private void Hit()
+    {
+
+    }
+
+    private void Dead()
+    {
+
+    }
+
+    private void Pass()
+    {
+
+    }
+    #endregion
+
+    #region 事件
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+
+
+}
