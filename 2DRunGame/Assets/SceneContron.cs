bcd5d9d0ff65c820b880a2e4e0a68e15c0ac1037
yeyue101 <67904651+yeyue101@users.noreply.github.com>
2020-08-03 21:32:20 +0800

    認識判斷式

---
+++ 2DRunGame/Assets/SceneContron.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;   //引用 場景管理 API
+
+public class SceneContron : MonoBehaviour
+{
+    // 方法要被按鈕乎較需要設為公開 public
+
+    /// <summary>
+    /// 切換場景 
+    /// </summary>
+    public void ChangeScene()
+    {
+        //切換場景
+        //場景管理.載入場景("場景名稱")
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void Quit()
+    {
+        //關閉遊戲並離開
+        //應用程式,離開
+        Application.Quit();
+
+    }
+
+    //延遲呼叫方法 Invoke("方法名稱"，延遲秒數)
+    /// <summary>
+    /// 延遲切換場景
+    /// </summary>
+    public void DelayChangeScene()
+    {
+        invoke("ChangeScene", 0.7f);
+    }
+
+    // 練習：延遲呼叫離開遊戲
+
+}
---
+++ 2DRunGame/Assets/Scripts/Learnlf.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Learnlf : MonoBehaviour
+{
+    //判斷式 if
+    //判斷 布林植 來決定要執行哪種程式
+    //範例：
+    //判斷 玩家是否死亡 眼視遊戲結束
+
+    private void Start()
+    {
+        //語法：
+        //如果(布林植) {程式內容}
+        //當布林植等於 true 時才會執行 {} 內容
+        if (true)
+        {
+            print("我是判斷式");
+        }
+    }
+
+    public bool open;
+
+    private void Update()
+    { 
+        //語法:
+        //否則{程式內容}
+        //當布林植等於 false 時才會執行否則 {} 內容
+        //否則必須寫在 if 下方， 不能單獨使用
+        if(open)
+        {
+            print("開門~");
+        }
+        else
+        {
+            print("關門~");
+        }
+    }
+
+
+
+
+
+}
